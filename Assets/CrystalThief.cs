5d496fc05c4e862a296f2f1b7b2b54eabe732612
KasTsuAl <yy90987723@gmail.com>
2022-03-08 23:27:09 +0800

    水晶小偷初版

---
+++ Assets/CrystalThief.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalThief : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] waypoints;
+    private int curwaypoint;
+}
