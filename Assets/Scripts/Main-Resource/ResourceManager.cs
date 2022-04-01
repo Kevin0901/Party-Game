c60a56fb43eea91b4b981bb8d756266881733c74
翁敬堯 <30209arthur@gmail.com>
2022-04-01 12:03:55 +0800

    .

--- Assets/Scripts/Main-Resource/ResourceManager.cs
+++ Assets/Scripts/Main-Resource/ResourceManager.cs
@@ -17,6 +17,7 @@ public class ResourceManager : MonoBehaviour
     private float timerMax = 1f;
     private int c = 0;
     public DatabaseReference reference;
+    private DataSnapshot RedResData, BlueResData;
     private void Awake()
     {
         Instance = this;
@@ -28,8 +29,8 @@ public class ResourceManager : MonoBehaviour
 
         foreach (ResourceTypeSo resourceType in resourceTypeList.list)
         {
-            reference.Child("GameRoom").Child(PhotonNetwork.CurrentRoom.Name).Child("RedResource").Child("resourceType.ToString()").SetValueAsync(100);
-            reference.Child("GameRoom").Child(PhotonNetwork.CurrentRoom.Name).Child("BlueResource").Child("resourceType.ToString()").SetValueAsync(100);
+            reference.Child("GameRoom").Child("123456").Child("RedResource").Child(resourceType.ToString()).SetValueAsync(100);
+            reference.Child("GameRoom").Child("123456").Child("BlueResource").Child(resourceType.ToString()).SetValueAsync(100);
             RedresourceAmountDictionary[resourceType] = 100;
             BlueresourceAmountDictionary[resourceType] = 100;
 
@@ -62,20 +63,28 @@ public class ResourceManager : MonoBehaviour
 
     public void AddResource(ResourceTypeSo resourceType, int amount)
     {
-        RedresourceAmountDictionary[resourceType] += amount;
-        BlueresourceAmountDictionary[resourceType] += amount;
+        updateResource();
+        // int RedresValue = Convert.ToInt32(RedResData.Child(resourceType.ToString()).Value);
+        // reference.Child("GameRoom").Child("123456").Child("RedResource").Child(resourceType.ToString()).SetValueAsync(RedresValue + amount);
+        Debug.Log("2");
+        Debug.Log(RedResData.ChildrenCount);
+
+        // int BlueresValue = Convert.ToInt32(BlueResData.Child(resourceType.ToString()).Value);
+        // reference.Child("GameRoom").Child("123456").Child("BlueResource").Child(resourceType.ToString()).SetValueAsync(BlueresValue + amount);
+        // Debug.Log(BlueResData.Child(resourceType.ToString()).Value);
 
         OnResourceAmountChanged?.Invoke(this, EventArgs.Empty);
-
-        // if (OnResourceAmountChanged != null) {
-        //     OnResourceAmountChanged(this, EventArgs.Empty);
-        // }
     }
 
     public void RedAddResource(ResourceTypeSo resourceType, int amount)
     {
-        RedresourceAmountDictionary[resourceType] += amount;
-
+        StartCoroutine(GetTeamResourceData((DataSnapshot Redinfo) =>
+        {
+            int resValue = Convert.ToInt32(Redinfo.Child(resourceType.ToString()).Value);
+            reference.Child("GameRoom").Child("123456").Child("RedResource").Child(resourceType.ToString()).SetValueAsync(resValue + amount);
+            reference.Child("GameRoom").Child("123456").Child("BlueResource").Child(resourceType.ToString()).SetValueAsync(resValue + amount);
+            Debug.Log(Redinfo.Child(resourceType.ToString()).Value);
+        }, (DataSnapshot Blueinfo) => { }));
         OnResourceAmountChanged?.Invoke(this, EventArgs.Empty);
 
         // if (OnResourceAmountChanged != null) {
@@ -85,8 +94,14 @@ public class ResourceManager : MonoBehaviour
 
     public void BlueAddResource(ResourceTypeSo resourceType, int amount)
     {
-        BlueresourceAmountDictionary[resourceType] += amount;
-
+        StartCoroutine(GetTeamResourceData((DataSnapshot Redinfo) => { }, (DataSnapshot Blueinfo) =>
+           {
+               int resValue = Convert.ToInt32(Blueinfo.Child(resourceType.ToString()).Value);
+               reference.Child("GameRoom").Child("123456").Child("RedResource").Child(resourceType.ToString()).SetValueAsync(resValue + amount);
+               reference.Child("GameRoom").Child("123456").Child("BlueResource").Child(resourceType.ToString()).SetValueAsync(resValue + amount);
+               Debug.Log(Blueinfo.Child(resourceType.ToString()).Value);
+           }
+        ));
         OnResourceAmountChanged?.Invoke(this, EventArgs.Empty);
 
         // if (OnResourceAmountChanged != null) {
@@ -116,11 +131,7 @@ public class ResourceManager : MonoBehaviour
     {
         foreach (ResourceAmount resourceAmount in resourceAmountsArray)
         {
-            if (RedGetResourceAmount(resourceAmount.resourceType) >= resourceAmount.amount)
-            {
-
-            }
-            else
+            if (RedGetResourceAmount(resourceAmount.resourceType) < resourceAmount.amount)
             {
                 return false;
             }
@@ -132,11 +143,7 @@ public class ResourceManager : MonoBehaviour
     {
         foreach (ResourceAmount resourceAmount in resourceAmountsArray)
         {
-            if (BlueGetResourceAmount(resourceAmount.resourceType) >= resourceAmount.amount)
-            {
-
-            }
-            else
+            if (BlueGetResourceAmount(resourceAmount.resourceType) < resourceAmount.amount)
             {
                 return false;
             }
@@ -159,4 +166,36 @@ public class ResourceManager : MonoBehaviour
         }
     }
 
+    IEnumerator GetTeamResourceData(System.Action<DataSnapshot> RedonCallbacks, System.Action<DataSnapshot> BlueonCallbacks)  //從資料庫抓取此房間的所有資料
+    {
+        var RedresData = reference.Child("GameRoom").GetValueAsync();
+        RedresData = reference.Child("GameRoom").Child("123456").Child("RedResource").GetValueAsync();
+        var BlueresData = reference.Child("GameRoom").GetValueAsync();
+        BlueresData = reference.Child("GameRoom").Child("123456").Child("BlueResource").GetValueAsync();
+        yield return new WaitUntil(predicate: () => RedresData.IsCompleted && BlueresData.IsCompleted);
+        if (RedresData != null && BlueresData != null)
+        {
+            DataSnapshot Redsnapshot = RedresData.Result;
+            DataSnapshot Bluesnapshot = BlueresData.Result;
+            RedonCallbacks.Invoke(Redsnapshot);
+            BlueonCallbacks.Invoke(Bluesnapshot);
+        }
+    }
+
+    private void updateResource()
+    {
+        Debug.Log("3");
+        StartCoroutine(GetTeamResourceData((DataSnapshot Redinfo) =>
+       {
+           RedResData = Redinfo;
+           Debug.Log(RedResData);
+           Debug.Log("1");
+       }, (DataSnapshot Blueinfo) =>
+         {
+           BlueResData = Blueinfo;
+           Debug.Log(BlueResData);
+           Debug.Log("1.5");
+       }
+       ));
+    }
 }
