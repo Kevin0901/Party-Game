50d68a2b05d2105bfdbcc94f0e55af17310a1c72
Ekko <48669764+jayleo0813@users.noreply.github.com>
2022-04-06 18:41:05 +0800

    0406

--- Assets/Scripts/FightArena/Ares/damageSpear.cs
+++ Assets/Scripts/FightArena/Ares/damageSpear.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class damageSpear : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.layer == 10)
         {
-            other.GetComponent<arenaPlayer>().hurt(1.5f);
+            other.gameObject.GetComponent<arenaPlayer>().hurt(1.5f);
         }
     }
 }
