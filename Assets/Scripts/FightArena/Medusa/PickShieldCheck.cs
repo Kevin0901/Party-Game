a7119a58cf0f599531a8501db1a62f6b2d5e83f0
Ekko <48669764+jayleo0813@users.noreply.github.com>
2022-05-19 00:25:11 +0800

    445541

--- Assets/Scripts/FightArena/Medusa/PickShieldCheck.cs
+++ Assets/Scripts/FightArena/Medusa/PickShieldCheck.cs
@@ -14,7 +14,9 @@ public class PickShieldCheck : MonoBehaviour
         {
             Find_Parent_Player = PhotonView.Find((int)PV.InstantiationData[0]).gameObject;
             Find_Parent_Player.transform.Find("shield").gameObject.SetActive(true);
+            Destroy(PhotonView.Find((int)PV.InstantiationData[1]).gameObject);
         }
+        
         Destroy(this.gameObject);
     }
 }
