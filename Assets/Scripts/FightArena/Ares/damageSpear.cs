57eb00df1bdbb9c096079a7d9a642b56fbc2eb5b
Ekko <48669764+jayleo0813@users.noreply.github.com>
2022-05-19 02:03:16 +0800

    更

--- Assets/Scripts/FightArena/Ares/damageSpear.cs
+++ Assets/Scripts/FightArena/Ares/damageSpear.cs
@@ -6,10 +6,12 @@ public class damageSpear : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other)
     {
+        Debug.Log(this.GetComponentInParent<spear>().isAttack);
         if (other.gameObject.layer == 10 && this.GetComponentInParent<spear>().isAttack)
         {
-            this.GetComponentInParent<spear>().isAttack = false;
+            Debug.Log(this.GetComponentInParent<spear>().isAttack);
             other.gameObject.GetComponent<arenaPlayer>().hurt(1f);
+            this.GetComponentInParent<spear>().isAttack = false;
         }
     }
 }
